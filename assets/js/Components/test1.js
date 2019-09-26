919879d74b6c11a1254a324bf16c0475931846eb
TessBeur <tess.beurskenshotmail.com>
2019-09-26 12:18:59 +0200

    derde

--- assets/js/Components/test1.js
+++ assets/js/Components/test1.js
@@ -1,212 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-
-let vogels = [
-  {
-    id: 1,
-    name: 'Natarajah',
-   
-  },
-  {
-    id: 2,
-    name: 'Krot',
-   
-  },
-  {
-    id: 3,
-    name: 'Mandala',
-   
-  },
-  {
-    id: 4,
-    name: 'Shiva',
-   },
-  {
-    id: 5,
-    name: 'Ashvattha',
-    }
-]
-
-let Vogels = React.createClass({
-   
-  render () {
-    return (
-      <li>
-        <span>{this.props.name}</span>
-        <span className="phone">{this.props.phone}</span>
-      </li>     
-    )
-  }
-})
-
-let VogelsList = React.createClass({
-  getInitialState () {
-    return {
-      displayedVogels:vogels      
-    };
-  },
-  searchHandler (event) {
-    let searcjQery = event.target.value.toLowerCase(),
-        displayedVogels = vogels.filter((el) => {
-          let searchValue = el.name.toLowerCase();
-          return searchValue.indexOf(searcjQery) !== -1;
-        })
-    this.setState({
-      displayedVogels: displayedVogels,
-      text:searcjQery
-    })
-  },
-  suggestionSelected(value){
-    this.setState(()=>({
-      text:value,
-      displayedVogels:[],
-    
-    }))
-   
-  },
-  renderSuggestions(){
-    let Vogels = this.state.displayedVogels;
-    const lengte= vogels.length;
-    
-    if (Vogels.length ===lengte){
-      return null;
-    }  
-      return(
-
-        <div style={{position: 'absolute', backgroundColor: "#ffff", width: 150 }}>
-        <table className="table table-hover"style={{ border: "1px solid grey", marginBottom: 0}} >
-          <tbody>
-            {Vogels.map((el)=><tr onClick={()=>this.suggestionSelected(el.name)}>{el.name}</tr>)}
-          </tbody>
-        </table>
-        </div>
-      )
-  },
-  render () {
-    console.log(this.state.text)
-    const {text} =this.state;
-    return (
-      <div className="holder">
-        <input  value={text} type="text" className="search" onChange={this.searchHandler}/>
-        <ul>
-          {this.renderSuggestions()}
-                    
-        </ul>
-      </div>     
-    )
-  }
-});
-
-ReactDOM.render(<VogelsList/>,document.getElementById('id'))
-
-
-
-
-// import React from 'react';
-// import '../../css/app.css';
-
-// export default class Search extends React.Component{
-//   constructor(props){
-//     super(props);
-//     this.items=[
-//       // {nr:1, soort:"vink1"},
-//       // {nr:2, soort:"vink2"},
-//       // {nr:3, soort:"vink3"},
-//       "vink1",
-//       "vink2",
-//       "vink3",
-//     ]
-
-//     this.state={
-//       suggestions:[],
-//       text:'',
-//     };
-//     this.OnTextChange = this.OnTextChange.bind(this);
-//   }
-
-//   OnTextChange(e){
-//     //const {items}=this.props    
-//     const value = e.target.value;
-//     let suggestions=[];
-//     if(value.length>0){
-//       const regex=new RegExp(`^${value}`,'i');
-//       //console.log(regex.test)
-//       suggestions=this.items.sort().filter(v => regex.test(v));
-//       this.setState(()=>({suggestions}));
-//     }
-//     this.setState(()=>({suggestions, text:value}));
-//   }
-
-//   suggestionSelected(value){
-//     this.setState(()=>({
-//       text:value,
-//       suggestions: [],
-//     }))
-//   }
-
-//   renderSuggestions(){
-//     const {suggestions}=this.state;
-//     if (suggestions.length === 0){
-//       return null;
-//     }
-//     return(
-//       <div style={{position: 'absolute', backgroundColor: "#ffff", width: 150 }}>
-//       <table className="table table-hover"style={{ border: "1px solid grey", marginBottom: 0}} >
-//         <tbody>
-//           {suggestions.map((item)=><tr onClick={()=>this.suggestionSelected(item)}>{item}</tr>)}
-//         </tbody>
-//       </table>
-//       </div>
-      
-//     )
-
-//   }
-//   render(){
-//     const {text} =this.state;
-//     console.log(this.items[0].soort)
-//     return(
-      
-//         <div>
-//         <input  className="form-control" value={text} type="text" onChange={this.OnTextChange} placeholder="Zoek..."/>
-//         {this.renderSuggestions()}
-//       </div>
-     
-//     )
-//   }
-// }
-
-
-// // import React, { Component } from "react";
-
-// // class ToDo extends Component{
-// //   constructor(props) {
-// //     super(props);
-
-// //     this.state={
-// //       email:[{
-// //         from:"beheer@nederlandsezebravinkenclub.nl",
-// //         to:"",
-// //         name:"",
-// //         subject:" Uw aanmelding | NZC Show ",
-// //         message:"test"
-// //       }]
-// //     }
-    
-// //   }
-// //   render(){
-// //     // testen als beiden from goed ingevuld zijn
-   
-
-// //     return(
-      
-// //       <fieldset>
-// //       <button className="btn btn-primary btn-lg" id="submit" value="Submit" onClick={this.props.click}>Inschijven</button>
-// //       </fieldset>
-      
-// //     );
-// //   }
-// // }
-
-// // export default ToDo;
+import React, { createRef } from 'react';
+const list = [ ... ];
+const List = () => (
+  <ul>
+    {list.map(item => {
+      const ref = createRef();
+      const handleClick = () =>
+        ref.current.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      return (
+        <li
+          key={item.id}
+          ref={ref}
+          style={{ height: '250px', border: '1px solid black' }}
+        >
+          <div>{item.id}</div>
+          <div>{item.firstname}</div>
+          <div>{item.lastname}</div>
+          <div>{item.year}</div>
+          <button type="button" onClick={handleClick}>
+            Scroll Into View
+          </button>
+        </li>
+      );
+    })}
+  </ul>
+);
+export default List;
--- assets/js/Components/zoek.js
+++ assets/js/Components/zoek.js
@@ -7,60 +7,106 @@ export default class Search extends React.Component{
     this.state={
       text:'',
       displayedVogels:[],  
+      displayedTotal:[],
       klasnr:"",
       data: null,
-      vogel:""
+      vogel:"", 
+      isToggleOn:false,
+      data2:[
+        {id:1, name:"een"},
+        {id:2, name:"twee"},
+        {id:3, name:"drie"},
+        {id:4, name:"vier"},
+    ]
 
     };
     this.getInitialState = this.getInitialState.bind(this);
     this.searchHandler = this.searchHandler.bind(this);
+    this.renderSuggestions = this.renderSuggestions.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.showMessage = this.showMessage.bind(this);
+            
+ 
   }
 
 componentDidMount(){
   fetch('https://script.google.com/macros/s/AKfycbz3ZkC81OfuuBKPu00YEsUiLi11Qw-HH2nS123U5Wz0wiCMPmHO/exec')
       .then(response => response.json())
-      .then(data => this.setState({ data }));
+      .then(data => this.setState({ data:data.vogelindex }));
       
     }
 
   getInitialState () {
     return {
-      displayedVogels:this.state.data.vogelindex  
+      displayedVogels:this.state.data
     };
   }
+      
+  handleClick() {
+    this.setState(function(prevState) {
+        return {isToggleOn: !prevState.isToggleOn};
+    });
+}
+
   searchHandler (event) {
     let searcjQery = event.target.value.toLowerCase(),
-        displayedVogels = this.state.data.vogelindex.filter((el) => {
+        displayedVogels = this.state.data.filter((el) => {
           let searchValue = el.Omschrijving.toLowerCase();
           return searchValue.indexOf(searcjQery) !== -1;
         })
     this.setState({
       displayedVogels: displayedVogels,
-      text:searcjQery
+      text:searcjQery,
+      displayedTotal:this.state.data
     })
   }
   suggestionSelected(value){
     this.setState(()=>({
       text:value.Omschrijving,
       displayedVogels:[],
-      id:value.Klasse_nr
+      id:value.Klasse_nr     
     
     }))
     this.props.onUpdate([{klas:value.Klasse_nr, om:value.Omschrijving}]);
-    this.setState({klasnr:value.Klasse_nr, vogel:value.Omschrijving});
+    this.setState({klasnr:value.Klasse_nr, vogel:value.Omschrijving, isToggleOn:false});
   }
-  renderSuggestions(){
+  showMessage(e) {
+    console.log(this.state.data)
+    if(this.state.isToggleOn==true){
+        
+       return(
+           <table>
+               <tbody>
+               {this.state.data2.map(item=><tr key={item.id}>{item.name}</tr>)}
+                </tbody>
+           </table>
+       )
+      }else{
+          return null}
+    }
+  
+  renderSuggestions(elem){
+   
     if (this.state.text=='' && this.state.displayedVogels.length >0 ){
       this.setState(()=>({
             displayedVogels:[],
                }))
       return null;
-          
-    }
+    }else if(this.state.isToggleOn==true){
+        
+       return(
+        <div style={{position: 'absolute', backgroundColor: "#ffff", width: 320,}}>
+          <table className="table table-striped table-hover active"style={{ border: "1px solid grey", marginBottom: 0 }} >
+            <tbody style={{height:200,overflowY:"scroll",display:"block"}}>
+            {this.state.data.map((el)=><tr key={el.Klasse_nr} onClick={()=>this.suggestionSelected(el)}>{el.Omschrijving}</tr>)}
+            </tbody>
+          </table>
+        </div>
+       )}
       return(
-        <div style={{position: 'absolute', backgroundColor: "#ffff", width: 320 }}>
-        <table className="table table-striped table-hover active"style={{ border: "1px solid grey", marginBottom: 0}} >
-          <tbody>
+        <div style={{position: 'absolute', backgroundColor: "#ffff", width: 320,}}>
+        <table className="table table-striped table-hover active"style={{ border: "1px solid grey", marginBottom: 0 }} >
+           <tbody>{/*style={{height:200,overflowY:"auto",display:"block"}} */}
           {this.state.displayedVogels.map((el)=><tr key={el.Klasse_nr} onClick={()=>this.suggestionSelected(el)}>{el.Omschrijving}</tr>)}
           </tbody>
         </table>
@@ -72,8 +118,12 @@ componentDidMount(){
     const {text} =this.state;
     return (
       <td>
-          <input className="form-control" value={text} type="text" onChange={this.searchHandler} placeholder="Zoek..." name="soort"/>
+        
+         <td> <input className="form-control" value={text} type="text" onChange={this.searchHandler} placeholder="Zoek..." name="soort" id="2"/></td>
+         <td><button className="btn btn-succes" style={{paddingLeft:0, paddingRight:0}} onClick={this.handleClick}>˅</button></td>
           {this.renderSuggestions()}
+          
+          
       </td>
     )
   }
